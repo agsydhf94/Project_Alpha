4eac71213ab1f7e63922e45ddc4cd039a15371c5
YoungHoon Kang <agsydhf94@gmail.com>
2025-10-21 21:47:53 +0900

    feat: add CharacterLocomotionManager base and extend PlayerLocomotionManager
    - Introduce CharacterLocomotionManager with a virtual Awake() hook for shared locomotion behaviors
    - Make PlayerLocomotionManager inherit from the new base class
    - Add movement state fields to PlayerLocomotionManager; verticalMovement, horizontalMovement, moveAmount, and internal moveDirection
    - Cache PlayerManager in Awake() and call base.Awake()

---
+++ Assets/Scripts/Character/CharacterLocomotionManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Alpha
+{
+    public class CharacterLocomotionManager : MonoBehaviour
+    {
+        protected virtual void Awake()
+        {
+            
+        }
+    }
+}
--- Assets/Scripts/Character/Player/PlayerLocomotionManager.cs
+++ Assets/Scripts/Character/Player/PlayerLocomotionManager.cs
@@ -2,8 +2,23 @@ using UnityEngine;
 
 namespace Alpha
 {
-    public class PlayerLocomotionManager : MonoBehaviour
+    public class PlayerLocomotionManager : CharacterLocomotionManager
     {
+        private PlayerManager player;
+
+        public float verticalMovement;
+        public float horizontalMovement;
+        public float moveAmount;
+
+        private Vector3 moveDirection;
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            player = GetComponent<PlayerManager>();
+        }
+
         public void HandleAllMovement()
         {
 
