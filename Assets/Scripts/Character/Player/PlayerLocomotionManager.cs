73bc56bba91cccd3414bf2b9d98d169b220bf5f8
YoungHoon Kang <agsydhf94@gmail.com>
2025-10-30 23:25:01 +0900

    feat: add camera-relative rotation handling to PlayerLocomotionManager
    - Add new rotation state/params in PlayerLocomotionManager
    
    - Add HandleRotation():
        - Build a camera-relative facing direction using PlayerCamera.instance.cameraObject.transform.forward / .right
        - Fall back to current transform.forward when there's no movement input
        - Smoothly rotate the player toward that direction using Quaternion.Slerp with rotationSpeed
    
    - Update HandleAllMovement() to call both HandleGroundedMovement() and the new HandleRotation() each frame

--- Assets/Scripts/Character/Player/PlayerLocomotionManager.cs
+++ Assets/Scripts/Character/Player/PlayerLocomotionManager.cs
@@ -11,9 +11,10 @@ namespace Alpha
         public float moveAmount;
 
         private Vector3 moveDirection;
-
+        private Vector3 targetRotationDirection;
         [SerializeField] private float walkingSpeed;
         [SerializeField] private float runningSpeed;
+        [SerializeField] private float rotationSpeed;
         
 
         protected override void Awake()
@@ -26,6 +27,7 @@ namespace Alpha
         public void HandleAllMovement()
         {
             HandleGroundedMovement();
+            HandleRotation();
         }
 
         private void GetVerticalAndHorizontalInputs()
@@ -37,7 +39,7 @@ namespace Alpha
         public void HandleGroundedMovement()
         {
             GetVerticalAndHorizontalInputs();
-            
+
             moveDirection = PlayerCamera.instance.transform.forward * verticalMovement;
             moveDirection += PlayerCamera.instance.transform.right * horizontalMovement;
             moveDirection.Normalize();
@@ -51,6 +53,22 @@ namespace Alpha
             {
                 player.characterController.Move(moveDirection * walkingSpeed * Time.deltaTime);
             }
+        }
+        
+        private void HandleRotation()
+        {
+            targetRotationDirection = Vector3.zero;
+            targetRotationDirection = PlayerCamera.instance.cameraObject.transform.forward * verticalMovement;
+            targetRotationDirection += PlayerCamera.instance.cameraObject.transform.right * horizontalMovement;
+
+            if (targetRotationDirection == Vector3.zero)
+            {
+                targetRotationDirection = transform.forward;
+            }
+
+            Quaternion newRotation = Quaternion.LookRotation(targetRotationDirection);
+            Quaternion targetRotation = Quaternion.Slerp(transform.rotation, newRotation, rotationSpeed * Time.deltaTime);
+            transform.rotation = targetRotation;
         }
     }
 }
